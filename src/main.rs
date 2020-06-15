c5b09315c1c82c61834502dd216789bf59069e01
Nils Olsson <nilso@enosis.net>
2020-06-15 14:12:27 -0700

    Lazy update

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,12 @@
 use std::convert::From;
 use std::fmt::Display;
 use std::fs::File;
-use std::ops::{Add, Mul};
 use std::path::Path;
 use std::{io, io::Write};
 
-struct Vec3 {
-    x: f64,
-    y: f64,
-    z: f64,
-}
+mod vector;
+use vector::Vec3;
 
-impl Add for Vec3 {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-            z: self.z + rhs.z,
-        }
-    }
-}
-
-impl Add<f64> for Vec3 {
-    type Output = Self;
-
-    fn add(self, rhs: f64) -> Self::Output {
-        Self {
-            x: self.x + rhs,
-            y: self.y + rhs,
-            z: self.z + rhs,
-        }
-    }
-}
-
-impl Mul<f64> for Vec3 {
-    type Output = Self;
-
-    fn mul(self, rhs: f64) -> Self::Output {
-        Self {
-            x: self.x * rhs,
-            y: self.y * rhs,
-            z: self.z * rhs,
-        }
-    }
-}
 
 // trait AsColor {
 //     fn as_color(&self) -> String;
---
+++ src/vector.rs
@@ -0,0 +1,43 @@
+use std::ops::{Add, Mul};
+
+pub struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Add<f64> for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x + rhs,
+            y: self.y + rhs,
+            z: self.z + rhs,
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
